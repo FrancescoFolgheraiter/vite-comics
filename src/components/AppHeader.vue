3274208777e754d92b46dce3a75279a7dfbbbf02
FrancescoFolgheraiter <f.folgheraiter@hotmail.it>
2024-01-18 17:38:01 +0100

    creata struttura header

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -2,7 +2,18 @@
 export default{
 	data() {
 		return{
-			text:"Header"
+			navBar:[
+				"CHARACTERS",
+				"COMICS",
+				"MOVIES",
+				"TV",
+				"GAMES",
+				"COLLECTIBLES",
+				"VIDEOS",
+				"FANS",
+				"NEWS",
+				"SHOP"
+			]
 		};
 	},
 	components:{
@@ -16,11 +27,34 @@ export default{
 </script>
 
 <template>
-	<header>
-		{{ text }}
+	<header class="d-flex debug">
+		<div id="logo">
+			<img src="/img/dc-logo.png" alt="">
+		</div>
+		<nav>
+			<ul class="d-flex">
+				<li v-for="(elem,i) in navBar">
+					{{ elem }}
+				</li>
+			</ul>
+		</nav>
 	</header>
 </template>
 
 <style lang ="scss" scoped>
-
+header{
+	width:1200px;
+	margin:0 auto;
+	justify-content: space-between;
+}
+ul{
+	list-style: none;
+}
+li{
+	margin:auto 10px;
+}
+.d-flex{
+	display:flex;
+	flex-wrap: wrap;
+}
 </style>
